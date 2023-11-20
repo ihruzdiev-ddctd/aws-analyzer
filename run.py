48f1241e9cc2d73fb723c97d80c74fd4a8c97ed5
Ihor Hruzdiev <ihruzdiev@dedicatted.com>
2023-11-20 18:42:14 +0200

    Updated variables

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from services.ec2 import elb, ec2, security_groups, snapshots
 from services.vpc import acl, igw, subnets, nat, route_tables
 
-snapshot_threshold = 365
-ec2_threshold = 90
+SNAPSHOT_THRESHOLD = 365
+EC2_THRESHOLD = 90
 
 
 def check_vpc() -> None:
@@ -15,8 +15,8 @@ def check_vpc() -> None:
 
 def check_ec2() -> None:
     elb.get_unused_lbs()
-    snapshots.get_old_snapshots(snapshot_threshold)
-    ec2.get_unused_instances(ec2_threshold)
+    snapshots.get_old_snapshots(SNAPSHOT_THRESHOLD)
+    ec2.get_unused_instances(EC2_THRESHOLD)
     security_groups.get_unused_security_groups()
 
 
