1ae2c737b25f4d5c37c90c9c6abcafcd2433cd9d
Ihor Hruzdiev <ihruzdiev@dedicatted.com>
2023-11-21 01:01:55 +0200

    Moved security groups to vpc, added docstring quotations

--- run.py
+++ run.py
@@ -1,28 +1,32 @@
 """Outputs unused EC2 and VPC resource's information"""
-from services.ec2 import elb, ec2, security_groups, snapshots
-from services.vpc import acl, igw, subnets, nat, route_tables
+from services.ec2 import elb, ec2, snapshots
+from services.vpc import acl, igw, subnets, nat, route_tables, security_groups
 
 SNAPSHOT_THRESHOLD = 365
 EC2_THRESHOLD = 90
 
 
 def check_vpc() -> None:
-    "Outputs VPC resource's informantion"
+    """
+    Outputs VPC resource's informantion
+    """
 
     acl.get_unused_acls()
     igw.get_unused_igws()
     subnets.get_unused_subnets()
     nat.get_unused_nat_gateways()
     route_tables.get_unused_route_tables()
+    security_groups.get_unused_security_groups()
 
 
 def check_ec2() -> None:
-    "Outputs EC2 resource's informantion"
+    """
+    Outputs EC2 resource's informantion
+    """
 
     elb.get_unused_lbs()
     snapshots.get_old_snapshots(SNAPSHOT_THRESHOLD)
     ec2.get_unused_instances(EC2_THRESHOLD)
-    security_groups.get_unused_security_groups()
 
 
 if __name__ == "__main__":
